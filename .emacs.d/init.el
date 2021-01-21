e4232157cc4c760661d664f305ccd9c745de55f0
Filipenco Pavel <filipencop@mail.ru>
2021-01-21 15:17:36 +0200

    minor tweak

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,7 +22,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company rainbow-delimiters rainbow-mode smart-tabs-mode sudo-edit avy diminish spaceline dashboard ido-vertical-mode smex evil gruvbox-theme)))
+   '(evil-org company rainbow-delimiters rainbow-mode smart-tabs-mode sudo-edit avy diminish spaceline dashboard ido-vertical-mode smex evil gruvbox-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
