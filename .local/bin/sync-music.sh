309e146ad9d4edbe71d35ccefdfe8503ad5479a9
Filipenco Pavel <filipencop@mail.ru>
2021-09-26 22:20:20 +0300

    Music syncer

---
+++ .local/bin/sync-music.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "make sure the phone's connected for file transfer."
+read uselessvar
+
+MOUNT_DIR="$(mktemp -d)"
+jmtpfs "$MOUNT_DIR"
+
+MUSIC_DIR="$MOUNT_DIR"/"Internal shared storage/Music/"
+
+rsync -rvu "$(xdg-user-dir MUSIC)/" "$MUSIC_DIR"
+sync
+fusermount -u "$MOUNT_DIR"
