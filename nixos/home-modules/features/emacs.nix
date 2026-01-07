0d9310d1c5336ddcd463c81952488fd4be89983a
Pavel Filipenco <filipencop@mail.ru>
2026-01-07 20:39:40 +0300

    emacs package tweaks

--- nixos/home-modules/features/emacs.nix
+++ nixos/home-modules/features/emacs.nix
@@ -1,12 +1,29 @@
 { lib, pkgs, config, inputs, ... }:
-let pkg =
-  (pkgs.emacs30.override {
+let
+  emacsPkg = (pkgs.emacs30.override {
     withNativeCompilation = true;
     withPgtk = true;
     withTreeSitter = true;
   });
-jdk = config.my.features.langs.java.jdk;
-langs = config.my.features.langs;
+  emacs = (pkgs.emacsPackagesFor emacsPkg).emacsWithPackages (
+    epkgs: with epkgs; [
+      (treesit-grammars.with-grammars (p: with p; [
+        tree-sitter-go
+        tree-sitter-nix
+        tree-sitter-java
+        tree-sitter-typescript
+        tree-sitter-vue
+        tree-sitter-erlang
+        tree-sitter-clojure
+        tree-sitter-dockerfile
+        tree-sitter-yaml
+        tree-sitter-proto
+        tree-sitter-typst
+      ]))
+    ]
+  );
+  jdk = config.my.features.langs.java.jdk;
+  langs = config.my.features.langs;
 in {
   options = {
     my.features.emacs.defaultEditor = lib.mkOption {
@@ -21,23 +38,19 @@ in {
 
   services.emacs = {
     enable = true;
-    package = pkg;
+    package = emacs;
     client.enable = true;
     defaultEditor = config.my.features.emacs.defaultEditor;
   };
 
   programs.emacs = {
     enable = true;
-    package = pkg;
-    extraPackages = (epkgs: with epkgs; [
-      tree-sitter
-      tree-sitter-langs
-      treesit-grammars.with-all-grammars
-    ]);
+    package = emacs;
   };
 
   home.packages = [
     pkgs.nixd
+    pkgs.clang
   ] ++ lib.optionals langs.rust.enable [
     (inputs.fenix.complete.withComponents [
       "cargo"
