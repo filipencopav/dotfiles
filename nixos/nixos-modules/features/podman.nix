527d7ba0edada74cdf98e6ba6a4c3e7dd1ef81a6
Pavel Filipenco <filipencop@mail.ru>
2026-02-16 21:23:36 +0300

    use alternate network backend for podman

--- nixos/nixos-modules/features/podman.nix
+++ nixos/nixos-modules/features/podman.nix
@@ -2,7 +2,10 @@
 {
   virtualisation = {
     containers.enable = true;
-    containers.containersConf.settings.engine."compose_warning_logs" = false;
+    containers.containersConf.settings = {
+      engine."compose_warning_logs" = false;
+      network.default_rootless_network_cmd = "slirp4netns";
+    };
     podman = {
       enable = true;
       defaultNetwork.settings.dns_enabled = true;
