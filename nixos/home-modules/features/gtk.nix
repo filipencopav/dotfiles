88d4e9287f87143af78b853ce153393e3182cf0b
Pavel Filipenco <filipencop@mail.ru>
2026-04-12 14:07:58 +0300

    nixos: remove redundant gtk icon theme package configuration

--- nixos/home-modules/features/gtk.nix
+++ nixos/home-modules/features/gtk.nix
@@ -1,14 +1,15 @@
 { pkgs, config, ... }:
-let xdg = config.xdg;
-in {
+let
+  xdg = config.xdg;
+in
+{
   stylix.targets.gtk.enable = true;
+  stylix.targets.gtk.fonts.enable = true;
   gtk = {
     enable = true;
 
     iconTheme = {
-      package = pkgs.kora-icon-theme.overrideAttrs (final: final // {
-        dontWrapQtApps = true;
-      });
+      package = pkgs.kora-icon-theme;
       name = "kora";
     };
 
@@ -18,6 +19,7 @@ in {
       gtk-application-prefer-dark-theme = 1;
     };
 
+    gtk4.theme = null;
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
